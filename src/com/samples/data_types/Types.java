40d0931218cd97bd8ad28af0688689a494b5d44e
talalUcef <tallal.youssef@gmail.com>
2016-01-02 18:23:41 +0000

    main project

---
+++ src/com/samples/data_types/Types.java
@@ -0,0 +1,7 @@
+package com.samples.data_types;
+
+/**
+ * Created by talalUcef on 02/01/2016.
+ */
+public class Types {
+}
---
+++ src/com/samples/hello_world/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.samples.hello_world;
+
+/**
+ * Created by talalUcef on 02/01/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
