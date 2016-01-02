f8d7275697bb121321bddb93658102752fd17b1a
talalUcef <tallal.youssef@gmail.com>
2016-01-02 20:27:56 +0000

    Using double example

---
+++ src/com/samples/data_types/examples/Area.java
@@ -0,0 +1,26 @@
+package com.samples.data_types.examples;
+
+import java.math.*;
+
+/**
+ * Created by talalUcef on 02/01/2016.
+ */
+public class Area {
+
+    // This program calculates the area of a circle
+    // Mathematicly the the erea a = pi * r^2 where r is the radius of the circle
+
+
+    public static void main(String[] args) {
+
+        // Circle*s radius
+        double r = 10.3453534;
+        // Circle's area
+        double a = Math.PI * r * r;
+
+        System.out.println("The area is "+ a);
+    }
+
+
+
+}
