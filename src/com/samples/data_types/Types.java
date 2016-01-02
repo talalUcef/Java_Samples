b4393cc83795aa14b75abb075bed7b0ba79929bf
talalUcef <tallal.youssef@gmail.com>
2016-01-02 18:53:15 +0000

    byte and short type

--- src/com/samples/data_types/Types.java
+++ src/com/samples/data_types/Types.java
@@ -3,5 +3,35 @@ package com.samples.data_types;
 /**
  * Created by talalUcef on 02/01/2016.
  */
+
+
 public class Types {
+
+    // Java is a strongly typed language
+
+    // Primitives types in java are :
+    //  # Integers : byle (8 bit), short (16 bis), int (32 bit) and long(64 bit)
+    //  # Floting point numbers : double(64 bit) and float (32 bit)
+    //  # Characters : char
+    //  # Boolean : boulean
+
+
+    public static void main( String[] args) {
+
+        // byte type (8 bits)
+        // Holds values between -2^7 and 2^7 -1
+        byte b1 = 23;
+        byte b2 = -18;
+        System.out.println("b1 = " + b1);
+        System.out.println("b2 = " + b2);
+
+        // short type (16 bits)
+        // Holds types between -2^15 and 2^15 -1
+        short s1 = 244;
+        short s2 = -334;
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+
+
+    }
 }
