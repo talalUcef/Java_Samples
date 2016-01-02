49ee69ea1dc2f24e9eda97102b887991786c87f6
talalUcef <tallal.youssef@gmail.com>
2016-01-02 18:59:30 +0000

    int type

--- src/com/samples/data_types/Types.java
+++ src/com/samples/data_types/Types.java
@@ -19,18 +19,27 @@ public class Types {
     public static void main( String[] args) {
 
         // byte type (8 bits)
-        // Holds values between -2^7 and 2^7 -1
+        // Holds integers between -2^7 and (2^7 - 1)
         byte b1 = 23;
         byte b2 = -18;
         System.out.println("b1 = " + b1);
         System.out.println("b2 = " + b2);
 
         // short type (16 bits)
-        // Holds types between -2^15 and 2^15 -1
+        // Holds integers between -2^15 and (2^15 - 1)
         short s1 = 244;
         short s2 = -334;
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
+
+        // int type (32 bits)
+        // Holds integers between -2^31 and (2^31 - 1)
+        int i1 = 3555;
+        int i2 = -434534;
+        System.out.println("i1 = " + i1);
+        System.out.println("i2 = " + i2);
+
+
 
 
     }
